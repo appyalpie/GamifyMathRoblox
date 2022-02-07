8d821b9ca44bcf343dda8aedf9e82fd4297c0630
ex2434 <ehorlocker@gmail.com>
2022-02-06 19:09:10 -0500

    Update MuteMusicInit.client.lua
    Created conditionals for muted and unmuted

--- src/StarterPlayerScripts/MuteMusicInit.client.lua
+++ src/StarterPlayerScripts/MuteMusicInit.client.lua
@@ -4,13 +4,15 @@ local playerGui = client:WaitForChild("PlayerGui")
 local screenGui = playerGui:WaitForChild("PersistentUI")
 local muteMusicButton = screenGui:FindFirstChildWhichIsA("TextButton")
 
-for i,v in pairs(playerGui:GetChildren()) do
-    print(v.Name)
-end
-
 muteMusicButton.Activated:Connect(function()
-    print('muteMusic hit')
-    local soundToMute = workspace.Sounds:FindFirstChild("Sound")
-    soundToMute:Pause()
-    muteMusicButton.Text = "Unmute Music"
+    local soundGroupToMute = workspace.Sounds:FindFirstChildWhichIsA("SoundGroup")
+    if soundGroupToMute.Volume ~= 0 then
+        soundGroupToMute.Volume = 0
+        muteMusicButton.Text = "Unmute Music"
+    end
+    if soundGroupToMute.Volume == 0 then
+        soundGroupToMute.Volume = 0.5
+        muteMusicButton.Text = "Mute Music"
+    end
+
 end)
