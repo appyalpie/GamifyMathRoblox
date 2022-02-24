bcb2f232d3c0a29f0ec73b6ee0c8bc3d8dc4c1ed
butteredfat <appyalpie@outlook.com>
2022-02-24 11:48:07 -0500

    Working in division / fractions

--- src/ServerScriptService/Island_2_Scripts/Game_24/CardObject.lua
+++ src/ServerScriptService/Island_2_Scripts/Game_24/CardObject.lua
@@ -1,3 +1,6 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+local MathUtilities = require(ServerScriptService.Utilities.MathUtilities)
+
 local Card = {}
 Card.__index = Card
 
@@ -16,19 +19,42 @@ function Card.new()
 	return self
 end
 
+function Card.getReducedFraction(a, b)
+
+end
+
 function Card.calculateValue(value)
 	if type(value) == "table" then
-		if value[1] then
-			if value[1] == "add" then
-				return Card.calculateValue(value[2]) + Card.calculateValue(value[3])
-			elseif value[1] == "subtract" then
-				return Card.calculateValue(value[2]) - Card.calculateValue(value[3])
-			elseif value[1] == "multiply" then
-				return Card.calculateValue(value[2]) * Card.calculateValue(value[3])
-			elseif value[1] == "divide" then
-				-- special case???????????????????????????????????????????????????????????????????????????????
-			end
-		end
+        
+
+        if value[1] == "add" then
+            return Card.calculateValue(value[2]) + Card.calculateValue(value[3])
+        elseif value[1] == "subtract" then
+            return Card.calculateValue(value[2]) - Card.calculateValue(value[3])
+        elseif value[1] == "multiply" then
+            local calculatedSecondValue = Card.calculateValue(value[2])
+            local calculatedThirdValue = Card.calculateValue(value[3])
+            if type(calculatedSecondValue) == "string" and type(calculatedThirdValue) == "string" then -- multiply two fractions
+                local firstFraction = calculatedSecondValue:split("/")
+                local secondFraction = calculatedThirdValue:split("/")
+                return MathUtilities.reduceFraction(firstFraction[1] * secondFraction[1], firstFraction[2] * secondFraction[2])
+            end
+            if type(calculatedSecondValue) == "string" then -- multiply fraction and number
+                local fraction = calculatedSecondValue:split("/")
+                return MathUtilities.reduceFraction(fraction[1] * calculatedThirdValue, fraction[2])
+            end
+            if type(calculatedThirdValue) == "string" then -- multiply number and fraction
+                local fraction = calculatedThirdValue:split("/")
+                return MathUtilities.reduceFraction(calculatedSecondValue * fraction[1], fraction[2])
+            end
+            return calculatedSecondValue * calculatedThirdValue -- multiply normally (number and number)
+        elseif value[1] == "divide" then
+            if Card.calculateValue(value[2]) % Card.calculateValue(value[3]) == 0 then
+                return Card.calculateValue(value[2]) / Card.calculateValue(value[3])
+            else -- returning a fraction
+                return tostring(Card.calculateValue(value[2])) .. "/" .. tostring(Card.calculateValue(value[3]))
+            end
+        end
 	end
 	return value
 end
--- src/ServerScriptService/Utilities/MathUtilities.lua
+++ src/ServerScriptService/Utilities/MathUtilities.lua
@@ -8,4 +8,24 @@ function MathUtilities.formatTime(timeLeft)
     return string.format("%02i", seconds)
 end
 
+-- greatest common devisor
+function MathUtilities.greatestCommonDivisor(a, b)
+    if b ~= 0 then
+        return MathUtilities.greatestCommonDivisor(b, a % b)
+    else
+        return math.abs(a)
+    end
+end
+
+-- fraction reduce
+function MathUtilities.reduceFraction(a, b)
+    local divisor = MathUtilities.greatestCommonDivisor(a, b)
+    a = a / divisor
+    b = b / divisor
+    if b == 1 then
+        return a
+    end
+    return a .. "/" .. b
+end
+
 return MathUtilities
