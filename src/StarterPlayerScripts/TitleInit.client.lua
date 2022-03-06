ca8ac212473ef025f72843c0a1fd510f5d949a1c
ex2434 <ehorlocker@gmail.com>
2022-03-06 18:36:24 -0500

    Update TitleInit.client.lua
    Moved around code and made sure titles aren't duplicated in the UI

--- src/StarterPlayerScripts/TitleInit.client.lua
+++ src/StarterPlayerScripts/TitleInit.client.lua
@@ -8,16 +8,11 @@ local titles = getTitles:InvokeServer(Players.LocalPlayer)
 
 local overheadTitle = Players.LocalPlayer.Character.Head:WaitForChild("overheadTitle")
 
+local Player = game:GetService("Players").LocalPlayer
+local InventoryGUI = Player:WaitForChild("PlayerGui"):WaitForChild("InventroyGUI"):WaitForChild("InventoryScreen")
+local TitleList = InventoryGUI:WaitForChild("TFrame")
 
--- This is just a small remoteevent that gets titles from the server
--- anytime a title is added (see TitleModule.AddTitle)
-local function onAddTitleFire(player)
-    titles = getTitles:InvokeServer()
-    print(titles)
-end
- 
-addTitleRepopulate.OnClientEvent:Connect(onAddTitleFire)
-
+-- TODO: apply title applied when the player left
 local function DisplayTitle(titleName)
     for key, value in pairs(titles) do
         if value == titleName then
@@ -26,20 +21,26 @@ local function DisplayTitle(titleName)
     end
 end
 
-DisplayTitle("Serf")
-
 --begin populate gui code
-local Player = game:GetService("Players").LocalPlayer
-local InventoryGUI = Player:WaitForChild("PlayerGui"):WaitForChild("InventroyGUI"):WaitForChild("InventoryScreen")
-local TitleList = InventoryGUI:WaitForChild("TFrame")
-
 local function addToFrame(title)
+    -- Check if the button already exists
+    local temp = TitleList:GetChildren()
+    for i = 1, #temp do
+        if temp[i]:IsA("TextButton") then
+            if temp[i].Text == title then
+                return
+            end
+        end
+    end
+    -- Create a new button, set parent to TitleList and text to the title
     local textButton = ReplicatedStorage:WaitForChild("InventoryScrollingButtonBasic"):Clone()
     textButton.Parent = TitleList
     textButton.Text = title
     if textButton.TextFits == false then
         textButton.TextScaled = true
     end
+    -- On activation, set "chosen" on all other buttons to false and
+    -- make other buttons background transparent
     textButton.Activated:Connect(function()
         DisplayTitle(textButton.text)
         local temp = TitleList:GetChildren()
@@ -53,6 +54,7 @@ local function addToFrame(title)
         textButton:SetAttribute("chosen", true)
     end)
 
+    -- When the mouse scrolls over the button, show a yellow background
     textButton.MouseEnter:Connect(function()
         if(textButton:GetAttribute("chosen") == false) then
             textButton.BackgroundTransparency = 0.9
@@ -66,7 +68,6 @@ local function addToFrame(title)
     end)
 end
 
-
 local function Populate()
     for i,title in pairs(titles) do
         addToFrame(title)
@@ -74,5 +75,22 @@ local function Populate()
 end
 
 Populate()
+--end populate gui code
+
+-- This is just a small remoteevent that gets titles from the server
+-- anytime a title is added (see TitleModule.AddTitle)
+local function onAddTitleFire(player)
+    titles = getTitles:InvokeServer()
+    Populate()
+    print(titles)
+end
+ 
+addTitleRepopulate.OnClientEvent:Connect(onAddTitleFire)
+
+
+
+
+
+
 
 
