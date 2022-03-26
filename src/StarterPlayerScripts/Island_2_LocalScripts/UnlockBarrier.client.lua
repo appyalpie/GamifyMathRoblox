3d81ca6a46a9661e94a6ac3cc8c30600ee6f90f8
butteredfat <appyalpie@outlook.com>
2022-03-26 14:46:50 -0400

    Update Barriers to unlock more

--- src/StarterPlayerScripts/Island_2_LocalScripts/UnlockBarrier.client.lua
+++ src/StarterPlayerScripts/Island_2_LocalScripts/UnlockBarrier.client.lua
@@ -1,13 +1,15 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local UnlockBarrierRE = ReplicatedStorage.RemoteEvents.Island_2:WaitForChild("UnlockBarrierRE")
-local Barrier = game.Workspace.Island_2.Barrier
+local Barriers = game.Workspace.Island_2.Barriers:GetChildren()
 
 UnlockBarrierRE.OnClientEvent:Connect(function()
     print("FIRED")
-    Barrier.Barrier_Part:Destroy()
-    Barrier.BeamHolder.Attachment0.Beam.Color = ColorSequence.new{
+    for _, v in pairs(Barriers) do
+        v.Barrier_Part:Destroy()
+        v.BeamHolder.Attachment0.Beam.Color = ColorSequence.new{
         ColorSequenceKeypoint.new(0, Color3.fromRGB(57, 194, 23)),
         ColorSequenceKeypoint.new(1, Color3.fromRGB(57, 194, 23))
     }
+    end
 end)
