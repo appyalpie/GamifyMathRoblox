1831ad19767c556929c3494fc2437fa99a621504
ex2434 <ehorlocker@gmail.com>
2022-04-10 14:22:30 -0400

    Changed GUI Reset on Spawn and Adjusted Code

--- src/StarterPlayerScripts/Island_3_LocalScripts/IngredientGUI.client.lua
+++ src/StarterPlayerScripts/Island_3_LocalScripts/IngredientGUI.client.lua
@@ -50,116 +50,8 @@ local playerIngredientInventory = {}
 
 
 -- This is made in case of player death, and will reinitialize variables
-local ReInitializeGUI = function()
-    IngredientGUI = Player:WaitForChild("PlayerGui"):WaitForChild("Island3GUI"):WaitForChild("IngredientFrame")
-
-    Ingredient1QuantityDisplay = IngredientGUI.InlayFrame.Ingredient1Frame:WaitForChild("Ingredient1Quantity")
-    Ingredient2QuantityDisplay = IngredientGUI.InlayFrame.Ingredient2Frame:WaitForChild("Ingredient2Quantity")
-    Ingredient3QuantityDisplay = IngredientGUI.InlayFrame.Ingredient3Frame:WaitForChild("Ingredient3Quantity")
-    
-    IngredientFrame = Player:WaitForChild("PlayerGui"):WaitForChild("Island3GUI"):WaitForChild("IngredientFrame")
-    
-    AddToPotionFrame = Player:WaitForChild("PlayerGui"):WaitForChild("Island3GUI"):WaitForChild("AddToPotionFrame")
-    AddIngredientsFrame = AddToPotionFrame:WaitForChild("InlayFrame"):WaitForChild("AddIngredientsFrame")
-    
-    CombineButton = AddToPotionFrame:WaitForChild("InlayFrame"):WaitForChild("CombineButton")
-    ExitButton = AddToPotionFrame:WaitForChild("ExitButton")
-    
-    AddIngredient1SubFrame = AddIngredientsFrame:WaitForChild("Ingredient1Subframe")
-    AddIngredient2SubFrame = AddIngredientsFrame:WaitForChild("Ingredient2Subframe")
-    AddIngredient3SubFrame = AddIngredientsFrame:WaitForChild("Ingredient3Subframe")
-    
-    Ingredient1IncrementAmountToAdd = AddIngredient1SubFrame:WaitForChild("AddIngredient")
-    Ingredient2IncrementAmountToAdd = AddIngredient2SubFrame:WaitForChild("AddIngredient")
-    Ingredient3IncrementAmountToAdd = AddIngredient3SubFrame:WaitForChild("AddIngredient")
-    
-    Ingredient1DecrementAmountToAdd = AddIngredient1SubFrame:WaitForChild("SubtractIngredient")
-    Ingredient2DecrementAmountToAdd = AddIngredient2SubFrame:WaitForChild("SubtractIngredient")
-    Ingredient3DecrementAmountToAdd = AddIngredient3SubFrame:WaitForChild("SubtractIngredient")
-    
-    Ingredient1AmountToAddTextBox = AddIngredient1SubFrame:WaitForChild("IngredientAmount")
-    Ingredient2AmountToAddTextBox = AddIngredient2SubFrame:WaitForChild("IngredientAmount")
-    Ingredient3AmountToAddTextBox = AddIngredient3SubFrame:WaitForChild("IngredientAmount")
-
-        ------ Plus button functionality on Combination Menu ------
-    Ingredient1IncrementAmountToAdd.Activated:Connect(function()
-        Ingredient1AmountToAddTextBox.Text = tonumber(Ingredient1AmountToAddTextBox.Text) + 1
-    end)
-
-    Ingredient2IncrementAmountToAdd.Activated:Connect(function()
-        Ingredient2AmountToAddTextBox.Text = tonumber(Ingredient2AmountToAddTextBox.Text) + 1
-    end)
-
-    Ingredient3IncrementAmountToAdd.Activated:Connect(function()
-        Ingredient3AmountToAddTextBox.Text = tonumber(Ingredient3AmountToAddTextBox.Text) + 1
-    end)
-
-    ------ Minus button functionality on Combination Menu ------
-    Ingredient1DecrementAmountToAdd.Activated:Connect(function()
-        Ingredient1AmountToAddTextBox.Text = tonumber(Ingredient1AmountToAddTextBox.Text) - 1
-    end)
-
-    Ingredient2DecrementAmountToAdd.Activated:Connect(function()
-        Ingredient2AmountToAddTextBox.Text = tonumber(Ingredient2AmountToAddTextBox.Text) - 1
-    end)
-
-    Ingredient3DecrementAmountToAdd.Activated:Connect(function()
-        Ingredient3AmountToAddTextBox.Text = tonumber(Ingredient3AmountToAddTextBox.Text) - 1
-    end)
-
-    ------ Text box validation - only positive numbers ------
-    Ingredient1AmountToAddTextBox:GetPropertyChangedSignal("Text"):Connect(function()
-        local newText = tonumber(Ingredient1AmountToAddTextBox.Text)
-        if newText == nil or newText < 0 then
-            newText = 0
-        end
-        Ingredient1AmountToAddTextBox.Text = newText;
-    end)
-
-    Ingredient2AmountToAddTextBox:GetPropertyChangedSignal("Text"):Connect(function()
-        local newText = tonumber(Ingredient2AmountToAddTextBox.Text)
-        if newText == nil or newText < 0 then
-            newText = 0
-        end
-        Ingredient2AmountToAddTextBox.Text = newText;
-    end)
-
-    Ingredient3AmountToAddTextBox:GetPropertyChangedSignal("Text"):Connect(function()
-        local newText = tonumber(Ingredient3AmountToAddTextBox.Text)
-        if newText == nil or newText < 0 then
-            newText = 0
-        end
-        Ingredient3AmountToAddTextBox.Text = newText;
-    end)
-
-    ------ Combine! Button Functionality ------
-    CombineButton.Activated:Connect(function()
-        -- Check to see if ingredients input is zero
-        if tonumber(Ingredient1AmountToAddTextBox.Text) == 0 and
-        tonumber(Ingredient2AmountToAddTextBox.Text) == 0 and
-        tonumber(Ingredient3AmountToAddTextBox.Text) == 0 then
-            CombineButton.Text = "Please put more than 0 Ingredients in!"
-            CombineButton.TextColor3 = Color3.new(1,0,0)
-            wait(2)
-            CombineButton.Text = "Combine!"
-            CombineButton.TextColor3 = Color3.new(0,0,0)
-            return
-        end
-
-        local selectedIngredients = {
-            Ingredient1 = tonumber(Ingredient1AmountToAddTextBox.Text), 
-            Ingredient2 = tonumber(Ingredient2AmountToAddTextBox.Text), 
-            Ingredient3 = tonumber(Ingredient3AmountToAddTextBox.Text)
-        }
-        CombinationButtonActivatedRE:FireServer(selectedIngredients)
-    end)
-
-    ------ Exit Button Functionality ------
-    ExitButton.Activated:Connect(function()
-        ExitButtonActivatedRE:FireServer()
-    end)
-
-    ------ Connect Death Functionality ------
+local InitializeDeathConnect = function()
+     ------ Connect Death Functionality ------
     game.Workspace:WaitForChild(Player.Name):WaitForChild("Humanoid").Died:Connect(function()
         local twinfo = TweenInfo.new(1,Enum.EasingStyle.Exponential,Enum.EasingDirection.Out,0,false,0)
         local goalPosition = {}
@@ -169,9 +61,88 @@ local ReInitializeGUI = function()
         tweenOut.Completed:Connect(function()
             IngredientFrame.Visible = false
         end)
-        GUIActivated = false
     end)
 end
+
+
+
+------ Plus button functionality on Combination Menu ------
+Ingredient1IncrementAmountToAdd.Activated:Connect(function()
+    Ingredient1AmountToAddTextBox.Text = tonumber(Ingredient1AmountToAddTextBox.Text) + 1
+end)
+
+Ingredient2IncrementAmountToAdd.Activated:Connect(function()
+    Ingredient2AmountToAddTextBox.Text = tonumber(Ingredient2AmountToAddTextBox.Text) + 1
+end)
+
+Ingredient3IncrementAmountToAdd.Activated:Connect(function()
+    Ingredient3AmountToAddTextBox.Text = tonumber(Ingredient3AmountToAddTextBox.Text) + 1
+end)
+
+------ Minus button functionality on Combination Menu ------
+Ingredient1DecrementAmountToAdd.Activated:Connect(function()
+    Ingredient1AmountToAddTextBox.Text = tonumber(Ingredient1AmountToAddTextBox.Text) - 1
+end)
+
+Ingredient2DecrementAmountToAdd.Activated:Connect(function()
+    Ingredient2AmountToAddTextBox.Text = tonumber(Ingredient2AmountToAddTextBox.Text) - 1
+end)
+
+Ingredient3DecrementAmountToAdd.Activated:Connect(function()
+    Ingredient3AmountToAddTextBox.Text = tonumber(Ingredient3AmountToAddTextBox.Text) - 1
+end)
+
+------ Text box validation - only positive numbers ------
+Ingredient1AmountToAddTextBox:GetPropertyChangedSignal("Text"):Connect(function()
+    local newText = tonumber(Ingredient1AmountToAddTextBox.Text)
+    if newText == nil or newText < 0 then
+        newText = 0
+    end
+    Ingredient1AmountToAddTextBox.Text = newText;
+end)
+
+Ingredient2AmountToAddTextBox:GetPropertyChangedSignal("Text"):Connect(function()
+    local newText = tonumber(Ingredient2AmountToAddTextBox.Text)
+    if newText == nil or newText < 0 then
+        newText = 0
+    end
+    Ingredient2AmountToAddTextBox.Text = newText;
+end)
+
+Ingredient3AmountToAddTextBox:GetPropertyChangedSignal("Text"):Connect(function()
+    local newText = tonumber(Ingredient3AmountToAddTextBox.Text)
+    if newText == nil or newText < 0 then
+        newText = 0
+    end
+    Ingredient3AmountToAddTextBox.Text = newText;
+end)
+
+------ Combine! Button Functionality ------
+CombineButton.Activated:Connect(function()
+    -- Check to see if ingredients input is zero
+    if tonumber(Ingredient1AmountToAddTextBox.Text) == 0 and
+    tonumber(Ingredient2AmountToAddTextBox.Text) == 0 and
+    tonumber(Ingredient3AmountToAddTextBox.Text) == 0 then
+        CombineButton.Text = "Please put more than 0 Ingredients in!"
+        CombineButton.TextColor3 = Color3.new(1,0,0)
+        wait(2)
+        CombineButton.Text = "Combine!"
+        CombineButton.TextColor3 = Color3.new(0,0,0)
+        return
+    end
+
+    local selectedIngredients = {
+        Ingredient1 = tonumber(Ingredient1AmountToAddTextBox.Text), 
+        Ingredient2 = tonumber(Ingredient2AmountToAddTextBox.Text), 
+        Ingredient3 = tonumber(Ingredient3AmountToAddTextBox.Text)
+    }
+    CombinationButtonActivatedRE:FireServer(selectedIngredients)
+end)
+
+------ Exit Button Functionality ------
+ExitButton.Activated:Connect(function()
+    ExitButtonActivatedRE:FireServer()
+end)
 
 local Populate = function()
     Ingredient1QuantityDisplay.Text = playerIngredientInventory["Ingredient1"]
@@ -240,34 +211,27 @@ CombineMenuFinishedRE.OnClientEvent:Connect(onCombineMenuFinishedEvent)
 
 ------ Called when the player has entered island 3 ------
 local function onIsland3EnteredEvent()
-    if GUIActivated == false then 
-        ReInitializeGUI()
-        Populate()
-        IngredientFrame.Visible = true
-        local twinfo = TweenInfo.new(1,Enum.EasingStyle.Exponential,Enum.EasingDirection.Out,0,false,0)
-        local goalPosition = {}
-        goalPosition.Position = UDim2.new(0.794,0,0.676,0)
-        local tweenIn = TweenService:Create(IngredientFrame ,twinfo, goalPosition)
-        tweenIn:Play()
-        GUIActivated = true
-    end
+    InitializeDeathConnect()
+    IngredientFrame.Visible = true
+    local twinfo = TweenInfo.new(1,Enum.EasingStyle.Exponential,Enum.EasingDirection.Out,0,false,0)
+    local goalPosition = {}
+    goalPosition.Position = UDim2.new(0.794,0,0.676,0)
+    local tweenIn = TweenService:Create(IngredientFrame ,twinfo, goalPosition)
+    tweenIn:Play()
 end
 
 Island3EnteredRE.OnClientEvent:Connect(onIsland3EnteredEvent)
 
 ------ Called when the player has entered a zone that's not island 3 ------
 local function onIsland3ExitEvent()
-    if GUIActivated == true then
-        local twinfo = TweenInfo.new(1,Enum.EasingStyle.Exponential,Enum.EasingDirection.Out,0,false,0)
-        local goalPosition = {}
-        goalPosition.Position = UDim2.new(1,0,0.676,0)
-        local tweenOut = TweenService:Create(IngredientFrame ,twinfo, goalPosition)
-        tweenOut:Play()
-        tweenOut.Completed:Connect(function()
-            IngredientFrame.Visible = false
-        end)
-        GUIActivated = false
-    end
+    local twinfo = TweenInfo.new(1,Enum.EasingStyle.Exponential,Enum.EasingDirection.Out,0,false,0)
+    local goalPosition = {}
+    goalPosition.Position = UDim2.new(1,0,0.676,0)
+    local tweenOut = TweenService:Create(IngredientFrame ,twinfo, goalPosition)
+    tweenOut:Play()
+    tweenOut.Completed:Connect(function()
+        IngredientFrame.Visible = false
+    end)
 end
 
 Island3ExitRE.OnClientEvent:Connect(onIsland3ExitEvent)
